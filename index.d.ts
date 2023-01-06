5744b07acc47a6b1e23c7209ca72389f37220c76
Kleyson Morais <kleyson@firetail.io>
2023-01-06 10:38:19 +0000

    Changed Definitions

--- index.d.ts
+++ index.d.ts
@@ -8,37 +8,42 @@
 // How to conntent it to the libs packageJson OR is it just based on the name above?
 
 type operation = {
-  [key: string]:operation | function
-}
+  [key: string]: operation | function;
+};
 
 type basic = {
-  user:string,
-  pass:string
-}
+  user: string;
+  pass: string;
+};
 type oauth2 = {
-  authorization:string,
-  scopes:object
-}
+  authorization: string;
+  scopes: object;
+};
 type jwt_key = {
-  authorization:string
+  authorization: string;
+};
+type CustomBodyDecoder = {
+  [key: string]: (body: string) => any;
+};
+type AuthCallbacks = {
+  [key: string]: authCallback;
+};
+
+function overRideError(err: object): object;
+function decodedJwt(headers: object): object;
+function authCallback(payload: basic | oauth2 | jwt_key): boolean | object;
+
+export interface FiretailOpts {
+  addApi: String;
+  dev?: Boolean;
+  specificationDir?: String;
+  overRideError?: overRideError;
+  operations?: operation;
+  decodedJwt?: decodedJwt;
+  authCallbacks?: AuthCallbacks;
+  customBodyDecoders?: CustomBodyDecoder;
 }
 
-function overRideError(err: object): object
-function decodedJwt(headers:object):object
-function authCallback(payload:basic|oauth2|jwt_key):boolean|object
-
-interface firetailOpts {
-  dev?:Boolean,
-  specificationDir?:String,
-  addApi:String,
-  overRideError?:overRideError,
-  operations?:operation,
-  decodedJwt?:decodedJwt,
-  authCallbacks:{
-    [key: string]:authCallback
-  }
-}
-
-function firetailSetup(firetailOpts):function
+function firetailSetup(opts: FiretailOpts): function;
 
 export = firetailSetup;
